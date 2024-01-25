4481d42d003edf943b50589a9e7983fad273f69f
ArthurEnrique15 <arthurenrique1512@gmail.com>
2024-01-25 12:16:31 -0300

    add eslint config file

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['@rocketseat/eslint-config/node'],
+  rules: {
+    camelcase: 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'prefer-const': 'off',
+    'no-import-assign': 'off',
+  },
+}
